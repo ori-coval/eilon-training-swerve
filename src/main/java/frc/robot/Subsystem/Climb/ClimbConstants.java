4104d74da1e684b4b2921bb4a5b1556739ad59df
Eilon <eilonhuppert@gmail.com>
2024-05-21 12:07:08 +0300

    intake

--- src/main/java/frc/robot/Subsystem/Climb/ClimbConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.Subsystem.Climb;
-
-public interface ClimbConstants {
-    int M_CLIMBINGRIGHT_MOTOR_ID = 0; //TODO: find id
-    int M_CLIMBINGLEFT_MOTOR_ID = 0; //TODO: find id
-    
-    
-}
