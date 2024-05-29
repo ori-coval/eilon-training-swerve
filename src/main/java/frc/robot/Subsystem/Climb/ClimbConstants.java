c1c8e329fa5eb1faffc705e26a8b79a4d37f8294
eilonhuppert <eilonhuppert@gmail.com>
2024-05-29 10:18:50 +0300

    bugs

--- src/main/java/frc/robot/Subsystem/Climb/ClimbConstants.java
+++ src/main/java/frc/robot/Subsystem/Climb/ClimbConstants.java
@@ -4,8 +4,6 @@ public interface ClimbConstants {
     int M_CLIMBINGRIGHT_MOTOR_ID = 0; //TODO: find id
     int M_CLIMBINGLEFT_MOTOR_ID = 0; //TODO: find id
 
-    double PALACE_MASTAKE = 5;
-
     int R_CURRENT_LIMIT = 40;
     int L_CURRENT_LIMIT = 40;
     
