fa16077aebd2516f4122f8882c18faba7f48d1c1
kully <oricoval@gmail.com>
2024-05-30 14:29:40 +0300

    fixed rebase

--- src/main/java/frc/robot/Subsystem/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystem/Intake/IntakeSubsystem.java
@@ -9,11 +9,7 @@ package frc.robot.Subsystem.Intake;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-
-import edu.wpi.first.units.Unit;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
