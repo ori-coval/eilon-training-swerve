449a6b6367cd3d7061c3587000ba67d82df2bfe2
eilonhuppert <eilonhuppert@gmail.com>
2024-06-14 12:03:29 +0300

    copy paste to swerve

--- src/main/java/frc/robot/subsystem/Vision/ApriltagCamera.java
+++ src/main/java/frc/robot/subsystem/Vision/ApriltagCamera.java
@@ -45,7 +45,7 @@ import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Robot;
 
-public class ApriltagCamera implements VisionConstants{
+public class AprilTagCamera implements VisionConstants{
     private PhotonCamera camera;
     private PhotonPoseEstimator photonEstimator;
     private double lastEstTimestamp = 0;
@@ -54,7 +54,7 @@ public class ApriltagCamera implements VisionConstants{
     private PhotonCameraSim cameraSim;
     private VisionSystemSim visionSim;
 
-    public ApriltagCamera (String cameraNmame){
+    public AprilTagCamera (String cameraNmame){
         camera = new PhotonCamera(cameraNmame);
 
         // stratagy/ idk how to write its 23:05 leave me alone 
