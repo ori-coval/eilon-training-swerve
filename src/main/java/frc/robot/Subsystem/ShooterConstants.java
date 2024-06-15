654a457943e9bf57d94448bf51970a78455359a8
eilonhuppert <eilonhuppert@gmail.com>
2024-06-15 17:57:23 +0300

    shooter configs

---
+++ src/main/java/frc/robot/Subsystem/ShooterConstants.java
@@ -0,0 +1,30 @@
+package frc.robot.Subsystem;
+
+public interface ShooterConstants {
+    int UP_MOTOR_ID = 0;
+    int DOWN_MOTOR_ID = 0;
+
+    //up configs
+    //FF
+    double UP_KS = 0;
+    double UP_KA = 0;
+    //PID
+    double UP_KP = 0;
+    double UP_KI =0;
+    double UP_KD = 0;
+
+    //down configs
+    double DOWN_KS = 0;
+    double DOWN_KA = 0;
+    //PID
+    double DOWN_KP = 0;
+    double DOWN_KI =0;
+    double DOWN_KD = 0;
+    //Targets
+    double ACCELERATION = 0;
+    double JERK = 0;
+    //limits
+    double CURRENT_LIMIT = 0;
+
+    
+}
