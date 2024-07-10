00c5d369475032445c79e713308f999d8e7fa7ef
eilonhuppert <eilonhuppert@gmail.com>
2024-07-10 17:31:47 +0300

    speaker pose

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,13 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class Constants {
 
     public static final String CAN_BUS_NAME = "canBus";
+    //field dimensions
+    public static final Translation2d BSpeakerPose = new Translation2d(0, 5.54);
+    public static final Translation2d RSpeakerPose = new Translation2d(16.39, 5.54);
+
+    
 }
